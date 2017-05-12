e30ceb3af8b095f2469146874ba032373f72b8a1
Arlen Keshabyan <arlen.albert@gmail.com>
2017-05-11 23:19:38 -0700

    live_property_example was refined a bit

--- examples/live_property_example.cpp
+++ examples/live_property_example.cpp
@@ -4,34 +4,43 @@
 
 int main()
 {
-	nstd::live_property<int> myInt{ "Test int" }, myInt2{ "!!!" };
-	std::vector<nstd::signalslot::connection> cons;
-	cons.emplace_back(
-    myInt.value_changing.connect([](auto &&prop, auto &&ctx)
+    using live_int = nstd::live_property<int>;
+
+	live_int int_prop{ "integer property for tests" }, dummy_int_prop{ "dummy" };
+	std::vector<nstd::signalslot::connection> conections;
+	conections.emplace_back(
+    int_prop.value_changing.connect([](auto &&prop, auto &&ctx)
     {
         std::cout << "The property '" << prop.name() << "' changing: from [" << prop << "] to [" << ctx.new_value << "]" << std::endl;
 
         if (ctx.cancel = ctx.new_value < 0; ctx.cancel)
         {
-            std::cout << "<<<negative integers are not allowed! The change was cancelled!>>>" << std::endl;
+            std::cout << "<<<negative integers are not allowed! The change was cancelled by a slot!>>>" << std::endl;
         }
     })
     );
-	cons.emplace_back(
-    myInt.value_changed.connect([](auto &&prop)
+	conections.emplace_back(
+    int_prop.value_changed.connect([](auto &&prop)
     {
         std::cout << "The property '" << prop.name() << "' changed to: " << prop << std::endl;
     })
     );
 
-	int t = 50;
-	myInt2 = 222;
-	myInt = 1;
-	myInt = 150;
-	myInt = t;
-	myInt = myInt2;
-	myInt = -1;
-	std::cout << myInt << std::endl;
+	int raw_int = 50;
+	dummy_int_prop = 222;
+	int_prop = 1;
+	int_prop = 150;
+	int_prop = raw_int;
+	int_prop = dummy_int_prop;
+	int_prop = -1;
+	std::cout << "int_prop = " << int_prop << std::endl;
+
+	conections.clear(); //auto-disconnect from all slots
+    std::cout << "no slots are called from now on since we destroied all connections..." << std::endl << "...setting int_prop to -1 now should work..." << std::endl;
+
+    int_prop = -1;
+
+    std::cout << "int_prop = " << int_prop << std::endl;
 
 	return 0;
 }
